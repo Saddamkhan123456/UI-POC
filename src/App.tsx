5c334ead6623fd17553c4ec947b1714e8781408f
Jyoti <Jyoti.thakre@talentica.com>
2022-05-10 20:11:25 +0530

    route config added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Product from "./pages/Product/Product";
-import Home from "./pages/Home/Home";
 import Header from "./common/Header/Header";
 import Footer from "./common/Footer/Footer";
+import RouteConfig from "./Route/RouteConfig";
 
 function App() {
   return (
     <>
       <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Product />} />
-        </Routes>
-      </BrowserRouter>
+        <RouteConfig/>
       <Footer />
     </>
   );
