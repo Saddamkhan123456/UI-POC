fbb8a7f32c9d990873e5b2c57ebc3481aebbde7d
Jyoti <Jyoti.thakre@talentica.com>
2022-06-28 19:45:48 +0530

    footer component

--- designsystem/src/atoms/footer/footer.tsx
+++ designsystem/src/atoms/footer/footer.tsx
@@ -8,78 +8,72 @@ export const Footer = ({ ...props }: FooterProps) => {
     { name: "Men", href: "#" },
     { name: "Women", href: "#" },
     { name: "Kids", href: "#" },
-   ];
+  ];
 
   const usefullink = [
-    {name: "Contact Us", href: "#"}, 
-    {name: "FAQ", href: "#"},
-  ]
+    { name: "Contact Us", href: "#" },
+    { name: "FAQ", href: "#" },
+  ];
   return (
     <>
-      <footer className="text-center lg:text-left bg-gray-100 text-gray-600">
-        <div className="mx-6 py-10 text-center md:text-left">
+      <footer className="pt-8 pb-4 px-1 text-center bg-theme-neutral flex flex-col justify-center items-center">
+        <div>
+          <img
+            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            alt="logo"
+            width={100}
+          />
+        </div>
+        <div className="my-4">
+          <ul className="flex flex-row">
+            {product.map((item, index) => (
+              <li  key={index}>
+                <a href={item.href} className="px-3 py-2 text-sm font-medium text-theme-neutral65">
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="text-center text-sm text-theme-neutral65">
+          <span>© 2022 Copyright:</span> 
+          <a className= "text-sm text-theme-neutral65 font-semibold" href="#">
+            Amazon
+          </a>
+        </div>
+        {/* <div className="mx-6 py-10 text-center md:text-left">
           <div className="grid grid-1 md:grid-cols-1 lg:grid-cols-3 gap-8">
-            <div className="">
-              <h1
-                className="
-            uppercase
-            font-semibold
-            mb-4
-            flex
-            items-center
-            justify-center
-            md:justify-start
-          "
-              >
-                LOGO
-              </h1>
-              <p>
-                About Text
-              </p>
-            </div>
             <div className="">
               <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
                 Products
               </h6>
-              {product.map((item, index) => (
-                    <p className="mb-4" key={index}>
-                      <a
-                        href={item.href}
-                        className="text-gray-600"
-                      >
-                        {item.name}
-                      </a>
-                    </p>
-                  ))}
-              
+              <ul>
+                {product.map((item, index) => (
+                  <li className="mb-4" key={index}>
+                    <a href={item.href} className="text-gray-600">
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </div>
             <div className="">
               <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
                 Useful links
               </h6>
-              {usefullink.map((item, index) => (
-                    <p className="mb-4" key={index}>
-                      <a
-                        href={item.href}
-                        className="text-gray-600"
-                      >
-                        {item.name}
-                      </a>
-                    </p>
-                  ))}
+              <ul>
+                {usefullink.map((item, index) => (
+                  <li className="mb-4" key={index}>
+                    <a href={item.href} className="text-gray-600">
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </div>
-            
           </div>
-        </div>
-        <div className="text-center p-1 bg-gray-200">
-          <span>© 2022 Copyright:</span>
-          <a
-            className="text-gray-600 font-semibold"
-            href="#"
-          >
-          ecommerce store
-          </a>
-        </div>
+        </div> */}
+       
       </footer>
     </>
   );
