0fe4665b417e2c2bb3898ae19f2ce975a15b2815
Abhishek Kumar <abhishek.kumar1@talentica.com>
2022-07-01 15:49:03 +0530

    first api

--- designsystem/src/page/homepage/HomePage.tsx
+++ designsystem/src/page/homepage/HomePage.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {allProducts} from '../../api/api'
+
+
 
 const HomePage = () => {
+  useEffect(() => {
+    const result = allProducts()
+    console.log(result)
+  }) 
   return <div>homePage</div>;
 };
 
