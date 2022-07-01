155965c3d5a9d378cb10f6750d74119e5a13a0b2
Jyoti <Jyoti.thakre@talentica.com>
2022-07-01 15:49:32 +0530

    Product details component

---
+++ designsystem/src/page/productDetails/ProductDetails.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default function ProductDetails() {
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-6">
+        <div className="flex flex-col md:flex-row -mx-4">
+          <div className="md:flex-1 px-4">
+            <div x-cloak>
+              <div className="mb-4">
+                <div className="mb-4 flex items-center justify-center">
+                   <img src="https://images.bewakoof.com/t1080/whatever-cat-boyfriend-t-shirt-388114-1655748484-1.jpg"
+                   alt="productImage"/>
+                </div>
+              </div>
+
+             
+            </div>
+          </div>
+          <div className="md:flex-1 px-4">
+            <h1 className="mb-2 text-theme-neutral uppercase text-2xl md:text-3xl">
+            DILLINGER
+            </h1>
+            <p className="mb-4 text-theme-neutral55">
+            Women Blue Printed Round Neck Oversized T-shirt
+            </p>
+            
+            <h5 className="mb-2 text-theme-neutral text-md">
+              Size
+            </h5>
+
+            <h5 className="mb-2 text-theme-neutral text-md">
+                Quantity
+            </h5>
+            <div className="">
+                
+            </div>
+
+            <h5 className="mb-2 text-theme-neutral text-md">
+                Description
+            </h5>
+            <div className="product-descriptiom">
+            <p className="mb-4 text-theme-neutral55">
+            Flex your cattitude with this Whatever Cat Women's Boyfriend T-shirt. Team this black t-shirt with high-waist jeans, funky sneakers and sling bag for a chic look.
+            </p>
+           
+            </div>
+
+            <div className="flex items-center space-x-4 my-4">
+              <div>
+                <div className="rounded-lg flex px-3">
+                  <span className="text-theme-neutral mr-1 mt-1">₹1000</span>
+                 
+                </div>
+              </div>
+              
+            </div>
+
+           
+
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
