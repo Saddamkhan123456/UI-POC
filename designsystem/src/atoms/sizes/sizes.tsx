500c3d7f1b65572b6b47ad99dab5a6fe2069ca61
Jyoti <Jyoti.thakre@talentica.com>
2022-07-04 13:25:44 +0530

    size component

---
+++ designsystem/src/atoms/sizes/sizes.tsx
@@ -0,0 +1,16 @@
+
+import React, { useState } from "react";
+import { HTMLAttributes } from "react";
+
+
+
+export interface SizesProps extends HTMLAttributes<HTMLElement> {}
+export const Sizes = ({ ...props }: SizesProps) => {
+ 
+  
+  return (
+    <div>
+       Size
+    </div>
+  );
+};
