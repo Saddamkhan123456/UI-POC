7484dde1fba6cbf1d0d355201da069ca327b08ac
vinil <vinil.pansare@talentica.com>
2022-07-06 16:37:58 +0530

    Revert "product details route configer"
    This reverts commit 05f70af69421417ed3857ac6c3fba3d20312b491.

--- designsystem/src/page/productDetails/ProductDetails.tsx
+++ designsystem/src/page/productDetails/ProductDetails.tsx
@@ -2,19 +2,19 @@ import React, { useEffect, useState } from "react";
 import { Button } from "design-system";
 import { QuantityBox } from "../../molecules/quantityBox";
 import { Sizes } from "../../atoms/sizes";
-import { useLocation } from "react-router-dom";
+import { allProducts } from "../../api/api";
 
-
-export default function ProductDetails(product) {
-  console.log("Product Details");
-  // const { search } = useLocation();
-  // const params = new URLSearchParams(search);
-  // const productid = params.get('productId'); // bar
-  // console.log(productid);
-
-  
-
+const ProductDetails = () => {
+  const [productData, setProductData] = useState([]);
+  useEffect(() => {
+    allProducts().then((response) => {
+      console.log(response["data"]);
+      setProductData(response["data"]);
+    });
+  }, []);
   return (
+    <>
+    {productData.map((item) => (
     <div>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-6">
         <div className="flex flex-col md:flex-row -mx-4">
@@ -23,19 +23,21 @@ export default function ProductDetails(product) {
               <div className="mb-4">
                 <div className="mb-4 flex items-center justify-center">
                   <img
-                    src="https://images.bewakoof.com/t1080/whatever-cat-boyfriend-t-shirt-388114-1655748484-1.jpg"
-                    alt="productImage" width="400"
+                    src={item.images[1]}
+                    alt={item.title}
+                   
+                    className="object-fill h-60 w-80"
                   />
                 </div>
               </div>
             </div>
           </div>
           <div className="md:flex-1 px-4">
-            <h1 className="mb-2 text-theme-neutral uppercase text-2xl md:text-3xl">
-              DILLINGER
+            <h1 className="mb-0 text-theme-neutral uppercase text-2xl md:text-3xl">
+              {item.title}
             </h1>
             <p className="mb-4 text-theme-neutral55">
-              Women Blue Printed Round Neck Oversized T-shirt
+              {item.category}
             </p>
 
             <h5 className="mb-2 text-theme-neutral text-md">Size</h5>
@@ -51,9 +53,7 @@ export default function ProductDetails(product) {
             <h5 className="mb-2 text-theme-neutral text-md">Description</h5>
             <div className="product-descriptiom">
               <p className="mb-4 text-theme-neutral55">
-                Flex your cattitude with this Whatever Cat Women's Boyfriend
-                T-shirt. Team this black t-shirt with high-waist jeans, funky
-                sneakers and sling bag for a chic look.
+                {item.description}
               </p>
             </div>
 
@@ -61,23 +61,35 @@ export default function ProductDetails(product) {
               <div>
                 <div className="rounded-lg flex">
                   <span className="text-theme-neutral mr-1 mt-1 text-3xl font-bold">
-                    ₹1000
+                  ${item.price}
                   </span>
                 </div>
               </div>
             </div>
-            <div className="flex items-center">
-              <Button variant="primary" size="medium" className="mr-2">
-                Add to card
-              </Button>
-              <Button variant="primary" size="medium">
-                Wishlist
-              </Button>
-            </div>
+            <div className="flex items-center  pb-4 ">
+                    <div className="w-1/2 md:w-1/3 pr-2">
+                      <Button
+                        variant="primary"
+                        className="block w-full px-3 py-2  uppercase"
+                      >
+                        wishlist
+                      </Button>
+                    </div>
+                    <div className="w-1/2 md:w-1/3 pr-2">
+                      <Button
+                        variant="secondary"
+                        className="block w-full px-3 py-2 uppercase "
+                      >
+                        Add to cart
+                      </Button>
+                    </div>
+                  </div>
           </div>
         </div>
       </div>
-      ;
     </div>
-  );
+       ))}
+    </>
+    );
 }
+export default ProductDetails;
