bf4e20b9b7f64264d83f2a8429eee365888209ef
jyoti <Jyoti.thakre@talentica.com>
2022-07-06 17:23:27 +0530

    routing added

--- designsystem/src/Route/RouteConfig.tsx
+++ designsystem/src/Route/RouteConfig.tsx
@@ -1,17 +1,36 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useParams } from "react-router-dom";
 import HomePage from "../page/homepage/HomePage";
 import Cart from "../page/cart/Cart";
 import ProductDetails from "../page/productDetails/ProductDetails";
 import Settings from "../page/setting/settings";
+import { allProducts } from "../api/api";
+
 
 export default function RouteConfig() {
+  const [products, setProducts] = useState([]);
+  let { productId } = useParams();
+  useEffect(() => {
+    allProducts().then((response) => {
+      setProducts(response["data"]);
+     });
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/cart" element={<Cart />} />
-      <Route path="/product-details" element={<ProductDetails />} />
+      {/* <Route path="/product-details" element={<ProductDetails />} /> */}
       <Route path="/settings" element={<Settings />} />
+      <Route
+          path="/product/:productId"
+          element={({ match }) => (
+            <ProductDetails
+              product={products.find(
+                (product) => String(product.id) === match.params.id
+              )}
+            />
+          )}
+        />
     </Routes>
   );
 }
