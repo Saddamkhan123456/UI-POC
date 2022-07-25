0cfcab26606f38824b0e7d724b8fe6a19e6b6ce4
palak <palak.amralkar@talentica.com>
2022-07-25 12:01:57 +0530

    added carousal to the landing page

--- designsystem/src/page/homepage/HomePage.tsx
+++ designsystem/src/page/homepage/HomePage.tsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { CategoryCard } from "../../molecules/categoryCard";
 import { categoryData, brandCategoryData } from "../../constants/constants";
+import CarouselComponent from "../../atoms/carousal/carousal";
 
 const HomePage = () => {
- 
   return (
     <>
       <div className="p-6 lg:p-6 sm:p-3">
+        <CarouselComponent />
         <div>
           <div className="mb-4">
             <CategoryCard
