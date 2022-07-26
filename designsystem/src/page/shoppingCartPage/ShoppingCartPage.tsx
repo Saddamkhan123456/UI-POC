77df6826591b1afba55a6c1cc2b50347bc7cb4a8
Jyoti <Jyoti.thakre@talentica.com>
2022-07-26 14:02:46 +0530

    added condition into qty

--- designsystem/src/page/shoppingCartPage/ShoppingCartPage.tsx
+++ designsystem/src/page/shoppingCartPage/ShoppingCartPage.tsx
@@ -15,6 +15,8 @@ const ShoppingCartPage = () => {
                 brand="Nike"
                 price="1294"
                 thumbnail="https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/13843398/2021/4/19/1847ce50-4f55-4bad-aaf0-de8a2b31fbbf1618820819735-Inddus-Teal-Blue-Solid-Ruffle-Accordion-Pleat-Saree-71616188-1.jpg"
+                qtyUpdate={true}
+                showQty={false}
               />
             </div>
           </div>
