10f382797dc66d6a3c2d6445e1586ffa070452cb
Jyoti <Jyoti.thakre@talentica.com>
2022-08-08 20:33:31 +0530

    checkout Form Ui

--- designsystem/src/page/checkoutPage/CheckoutPage.tsx
+++ designsystem/src/page/checkoutPage/CheckoutPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { OrderSummary } from "../../atoms/orderSummary";
 import { ShoppingCart } from "../../atoms/shoppingCart";
+import CheckoutForm from "./checkoutForm";
 
 const CheckoutPage = () => {
   return (
@@ -10,7 +11,7 @@ const CheckoutPage = () => {
           <div className="lg:w-2/3 sm:w-full">
             <div className="lg:px-6 sm:px-2 w-full">
               <h1 className="text-3xl font-black mb-12">Checkout</h1>
-              
+              <CheckoutForm />
             </div>
           </div>
           <div className="lg:w-1/3 md:w-1/3 sm:w-full">
