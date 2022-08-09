9951ebf4b10087540d4be28ffeac02904e93a971
Jyoti <Jyoti.thakre@talentica.com>
2022-08-09 16:30:41 +0530

    shopping cart

---
+++ designsystem/src/atoms/shoppingCart/shoppingCart.stories.tsx
@@ -0,0 +1,20 @@
+import { Story } from "@storybook/react";
+import { ShoppingCart, ShoppingCartProps } from "./index";
+const ShoppingComponent = {
+  title: "Design System/Atoms/ShoppingCart",
+  component: ShoppingCart,
+  
+};
+export default ShoppingComponent
+const Template: Story<ShoppingCartProps> = (args) =>
+ <ShoppingCart  {...args}
+ title="Black High Neck Cropped Top"
+ brand="Nike"
+ price="1294"
+ thumbnail="https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/13843398/2021/4/19/1847ce50-4f55-4bad-aaf0-de8a2b31fbbf1618820819735-Inddus-Teal-Blue-Solid-Ruffle-Accordion-Pleat-Saree-71616188-1.jpg"
+
+  />;
+
+export const BasicShopping = Template.bind({});
+
+
---
+++ designsystem/src/atoms/shoppingCart/shoppingCart.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { HTMLAttributes } from "react";
+import { Button } from "design-system";
+import { Link } from "react-router-dom";
+import { QuantityBox } from "../../molecules/quantityBox";
+export interface ShoppingCartProps extends HTMLAttributes<HTMLElement> {
+  title: string;
+  brand: string;
+  price: any;
+  thumbnail: any;
+  qtyUpdate:boolean;
+  showQty: boolean;
+}
+export const ShoppingCart = ({
+  title,
+  brand,
+  price,
+  thumbnail,
+  qtyUpdate,
+  showQty,
+}: ShoppingCartProps) => {
+  return (
+    <div className="flex flex-col">
+      <div className="flow-root">
+        <ul className="-my-6 divide-y">
+          <li className="flex flex-col md:flex-row py-8">
+            <div className="h-36 w-36 flex-shrink-0 overflow-hidden rounded-md border border-theme-neutral80">
+              <img
+                src={thumbnail}
+                alt={title}
+                
+                className="h-full w-full object-cover object-top"
+              />
+            </div>
+            <div className="lg:ml-4 sm:ml-0  flex flex-1 ">
+              <div className="w-full flex flex-col">
+                <div className="flex justify-between text-base text-theme-neutral  lg:mt-0  mt-2 flex-col md:flex-row">
+                  
+                    <h3>
+                      <a className="font-medium"> {title} </a>
+                      <p className="mt-1 mb-2 text-sm text-theme-neutral55 font-normal">
+                        {brand}
+                      </p>
+                    </h3>
+                    <p className="text-lg font-semibold">${price}</p>
+                </div>
+              
+                <div className="flex flex-1 items-end justify-between text-sm">
+                  <div>
+                    <p className="text-gray-500">Qty  {showQty ?<span>1</span> :''}</p>
+                    {qtyUpdate ?
+                    <QuantityBox />
+                     : ''}
+                  </div>
+                  <div className="flex">
+                    <div className="remove-button">
+                     
+                      <Button
+                        variant="secondary"
+                        size="small"
+                        className="font-medium text-theme-primary hover:text-theme-primary p-0"
+                      >
+                        Remove
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
