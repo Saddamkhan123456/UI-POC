fe70cd6dc03b7c8a258c2ad534d3a28f33e15d5c
Jyoti <Jyoti.thakre@talentica.com>
2022-08-17 23:14:09 +0530

    card UI fixes

--- designsystem/src/atoms/card/card.tsx
+++ designsystem/src/atoms/card/card.tsx
@@ -27,8 +27,8 @@ export const CardComponent = ({
   return (
     <>
       <>
-        <Card className="bg-transparent  justify-around group box-border overflow-hidden flex rounded-md cursor-pointer pe-0 pb-2 lg:pb-3 flex-col items-start transition duration-200 ease-in-out transform hover:-translate-y-1 hover:md:-translate-y-1.5 hover:shadow-lg">
-          <div className="mb-3 md:mb-3.5 flex w-full justify-center">
+        <Card className="bg-transparent  justify-around group box-border overflow-hidden flex rounded-md cursor-pointer pe-0  flex-col items-start transition duration-200 ease-in-out transform hover:-translate-y-1 hover:md:-translate-y-1.5 hover:shadow-lg mb-2">
+          <div className="mb-2 md:mb-2.5 flex w-full justify-center">
             <Link to={`/product/${id}`}>
               <img
                 src={thumbnail}
@@ -38,9 +38,9 @@ export const CardComponent = ({
               />
             </Link>
           </div>
-          <div className="rounded-b-md">
-            <div className="flex justify-between items-start px-2 pt-2 ">
-              <div className="p-2 flex-grow">
+          <div className="rounded-b-md p-2">
+            <div className="flex justify-between items-start">
+              <div className="mr-2 flex-grow">
                 <h2 className="text-base line-clamp leading-non">{title}</h2>
                 <p className="text-theme-neutral line-clamp-2 text-sm">
                   {description}
@@ -54,23 +54,23 @@ export const CardComponent = ({
                   ""
                 )}
               </div>
-              <div className="p-2 text-right">
+              <div className="ml-2 text-right">
                 <div className="text-theme-neutral">${price}</div>
               </div>
             </div>
-            <div className="flex justify-center items-center px-2 pb-2 ">
-              <div className="w-1/2 p-2">
+            <div className="flex justify-center items-center mt-2">
+              <div className="w-1/2 mr-2">
                 <Button
                   variant="primary"
-                  className="block w-full px-3 py-2  uppercase"
+                  className="block w-full px-3 py-2  uppercase h-38"
                 >
                   wishlist
                 </Button>
               </div>
-              <div className="w-1/2 p-2">
+              <div className="w-1/2 ml-2">
                 <Button
                   variant="secondary"
-                  className="block w-full px-3 py-2 uppercase "
+                  className="block w-full px-3 py-2 uppercase h-38"
                 >
                   <Icon kind="cart" size={16} />
                 </Button>
