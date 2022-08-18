8b4718dbe3b463fcab4db7ea13a0c8e6d95be416
Jyoti <Jyoti.thakre@talentica.com>
2022-08-18 14:33:37 +0530

    wishlist route remove

--- designsystem/src/Route/RouteConfig.tsx
+++ designsystem/src/Route/RouteConfig.tsx
@@ -4,7 +4,6 @@ import ProductDetails from "../page/productDetails/ProductDetails";
 import ProductCategoryPage from "../page/productCategoryPage/ProductCategoryPage";
 import ProductBrandPage from "../page/productBrandPage/ProductBrandPage";
 import ShoppingCartPage from "../page/shoppingCartPage/ShoppingCartPage";
-import Wishlist from "../page/wishlist/wishlist";
 import ThankYouPage from "../page/thankYouPage/ThankYouPage";
 import CheckoutPage from "../page/checkoutPage/CheckoutPage";
 import MyProfile from "../page/myProfile/myProfile";
@@ -20,7 +19,6 @@ export default function RouteConfig() {
       />
       <Route path="/product/brand/:brandId" element={<ProductBrandPage />} />
       <Route path="/shopping-cart" element={<ShoppingCartPage />} />
-      <Route path="/wishlist" element={<Wishlist />} />
       <Route path="/checkout" element={<CheckoutPage />} />
       <Route path="/thankyou" element={<ThankYouPage />} />
 
