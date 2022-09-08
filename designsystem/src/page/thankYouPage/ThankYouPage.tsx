088f03efe483a0a9e48dfab0ec6c506583640237
Jyoti <Jyoti.thakre@talentica.com>
2022-09-08 12:51:54 +0530

    thankyou ui fixes

--- designsystem/src/page/thankYouPage/ThankYouPage.tsx
+++ designsystem/src/page/thankYouPage/ThankYouPage.tsx
@@ -4,7 +4,7 @@ import ThankyouComponent from "../../atoms/thankyou/thankyou";
 
 const ThankYouPage = () => {
   return (
-    <div className="p-6 lg:p-6 sm:p-3 h-full">
+    <div className="flex flex-col container mx-auto h-full">
         <ThankyouComponent orderId="848485"/>
     </div>
   );
