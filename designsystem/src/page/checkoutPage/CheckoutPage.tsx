d5b7df68be1ac09d298c62470c537c9e5c4d00ef
Jyoti <Jyoti.thakre@talentica.com>
2022-09-08 09:28:22 +0530

    checkout ui fixes

--- designsystem/src/page/checkoutPage/CheckoutPage.tsx
+++ designsystem/src/page/checkoutPage/CheckoutPage.tsx
@@ -9,29 +9,12 @@ const CheckoutPage = () => {
   const { cartItems , saveCartItem , deleteCartItem , flushCartItem } = React.useContext(CartContext);
   const {saveWishlistItem} = React.useContext(WishlistContext)
   return (
-    <div className="p-6 lg:p-6 sm:p-3 overflow-auto">
-      <h1 className="text-3xl font-black mb-3">Checkout</h1>
+    <div className="flex flex-col p-3 container mx-auto">
+      <h1 className="mb-3 text-theme-neutral capitalize text-2xl mt-4">Checkout</h1>
       <div className="flex py-8">
         <div className="flex flex-col md:flex-row w-full overflow-auto">
           <div className="lg:w-2/3 sm:w-full">
-            <div className="w-full mt-8">
-              {cartItems.map((item) => {
-                return (
-                  <ItemCard
-                      key={item.id}
-                      cartItem={item}
-                      cartCard={false}
-                      imgSize={false}
-                      isQuantityShow={true}
-                      isCartItem={true}
-                      addItem={() => saveCartItem(item)}
-                      removeItem={() => deleteCartItem(item)}
-                      deleteItem={() => flushCartItem(item)}
-                      addToWishlist={() => saveWishlistItem(item)}
-                    />
-                );
-              })}
-            </div>
+            
             <div className="w-full">
               <CheckoutForm />
             </div>
