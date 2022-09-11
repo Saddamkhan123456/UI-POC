a994d26ed2c3babb14865702e485741c2441c2c7
Abhishek Kumar <abhishek.kumar1@talentica.com>
2022-09-11 15:58:15 +0530

    Refactored the code
    Pushed footer to the bottom
    checkout page style fixes

--- designsystem/src/page/checkoutPage/CheckoutPage.tsx
+++ designsystem/src/page/checkoutPage/CheckoutPage.tsx
@@ -4,17 +4,21 @@ import CheckoutForm from "./checkoutForm";
 
 const CheckoutPage = () => {
   return (
-    <div className="flex flex-col container mx-auto py-3 h-full">
+    <div className="flex flex-col container mx-auto p-3">
       <h1 className="text-xl capitalize leading-none pb-4 mb-4 border-b font-medium">
         Checkout
       </h1>
-      <div className="flex pb-5">
+      <div className="flex py-8">
         <div className="flex flex-col md:flex-row w-full overflow-auto">
-          <div className="w-3/5 flex justify-center">
+          <div className="lg:w-3/5 md:w-2/3 sm:w-full">
+            <div className="w-full">
             <CheckoutForm />
+            </div>
           </div>
-          <div className="w-2/5">
+          <div className="lg:w-2/5 md:w-1/3 sm:w-full">
+          <div className="w-full lg:pl-6 sm:pl-2">
             <OrderSummary showShoppingCart={true} isCheckout={false} />
+          </div>
           </div>
         </div>
       </div>
