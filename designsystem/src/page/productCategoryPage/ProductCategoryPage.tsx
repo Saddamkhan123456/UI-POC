bdb987ac3ae0e9e58e1ddc3a4e19b575e8d7395b
palak <palak.amralkar@talentica.com>
2022-09-14 13:49:29 +0530

    3. spacing corrections in category and brand page

--- designsystem/src/page/productCategoryPage/ProductCategoryPage.tsx
+++ designsystem/src/page/productCategoryPage/ProductCategoryPage.tsx
@@ -16,7 +16,7 @@ const ProductCategoryPage = () => {
   return (
     <div className="flex flex-col items-center">
       <Banner pageHeading={categoryId} />
-      <div className="flex flex-col container">
+      <div className="flex flex-col container my-4">
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {categoryData &&
             categoryData
