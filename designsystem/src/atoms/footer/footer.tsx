79c708111fc2efbf1eee6331178f69ab43263a55
Jyoti <Jyoti.thakre@talentica.com>
2022-09-15 15:56:06 +0530

    removed unnecessary files from footer

--- designsystem/src/atoms/footer/footer.tsx
+++ designsystem/src/atoms/footer/footer.tsx
@@ -1,10 +1,6 @@
-import { HTMLAttributes } from "react";
 import Icon from "../icons/icon";
-
-export interface FooterProps extends HTMLAttributes<HTMLElement> {}
-export const Footer = ({ ...props }: FooterProps) => {
-
-  return (
+export const Footer = ({ ...props }) => {
+ return (
     <>
       <footer className="p-4 text-center bg-theme-primary flex flex-col justify-center items-center mt-auto" {...props}>
         <div className="flex items-center">
