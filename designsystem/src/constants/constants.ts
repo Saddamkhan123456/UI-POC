da29916d11fb3e2f8e9e4ae6cfa50b053e23fa46
palak <45681524+palak-tal@users.noreply.github.com>
2022-09-15 15:26:09 +0530

    removed the current from navlink

--- designsystem/src/constants/constants.ts
+++ designsystem/src/constants/constants.ts
@@ -50,7 +50,7 @@ export const brandCategoryData: CardPropsSets[] = [
 
 
 export const navigation = [
-  { name: "Men", href: "/product/category/men", current: false },
-  { name: "Women", href: "/product/category/women", current: false },
-  { name: "Kids", href: "/product/category/kids", current: false },
+  { name: "Men", href: "/product/category/men"},
+  { name: "Women", href: "/product/category/women"},
+  { name: "Kids", href: "/product/category/kids"},
 ];
