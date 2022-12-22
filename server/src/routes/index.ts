2bb1f374aafb6c0403c5d25de51e9f6d8dfeaf28
saddamhusainkhan <saddamhusain.khan@talentica.com>
2022-12-22 17:53:28 +0530

    category api created

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,8 +3,10 @@ const router = Router();
 
 import authRoutes from './auth.routes';
 import userRoutes from './user.routes';
+import categoryRoutes from './category.routes';
 
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
+router.use('/category', categoryRoutes);
 
 export default router;
