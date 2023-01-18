22b06912818487a787d002f2b8dad18164b72ec9
Abhishek Kumar <abhishek.kumar1@talentica.com>
2023-01-18 18:08:49 +0530

    created Signup page and integrated auth api with Signup

--- designsystem/src/Route/Pages.tsx
+++ designsystem/src/Route/Pages.tsx
@@ -1,4 +1,11 @@
 export const Pages = {
-  HOMEPAGE : "/",
-  PRODUCT_DETAIL : "/product/:productId"
-}
+  HOMEPAGE: '/',
+  PRODUCT_DETAIL: '/product/:productId',
+  CATEGORY_DETAIL: '/product/category/:categoryId',
+  BRAND_DETAIL: '/product/brand/:brandId',
+  SHOPPING_CART: '/shopping-cart',
+  CHECKOUT: '/checkout',
+  THANKYOU: '/thankyou',
+  MYPROFILE: '/my-profile',
+  REGISTER: '/register',
+};
