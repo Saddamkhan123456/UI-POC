36520035363fbe4149deed906a7fe42e285affca
sujayUbhe <66360149+sujayUbhe@users.noreply.github.com>
2023-02-19 15:51:27 +0530

    Update wishlistComponent.tsx

--- designsystem/src/atoms/wishlist/wishlistComponent.tsx
+++ designsystem/src/atoms/wishlist/wishlistComponent.tsx
@@ -51,9 +51,11 @@ export const WishlistComponent = ({
         >
           <div>
             <Icon kind="wishlist" size={20} />
-            <span className="badge-count text-white bg-red-700 absolute rounded-full text-xs -mt-7 ml-2 py-0 px-1.5">
-              {wishlistData?.length}
-            </span>
+            {wishlistData?.length > 0 && (
+              <span className="badge-count text-white bg-red-700 absolute rounded-full text-xs -mt-7 ml-2 py-0 px-1.5">
+                {wishlistData?.length}
+              </span>
+            )}
           </div>
         </div>
         <OffCanvasComponent
