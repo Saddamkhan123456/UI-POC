056acdf3b406b070e6412a7e2d77f6b57cfd532a
Jyoti Thakre <jyoti.thakre@talentica.com>
2023-07-21 22:44:00 +0530

    Bookmarked Class updates

--- css-modules/src/Components/Molecules/Bookmarked/Bookmarked.tsx
+++ css-modules/src/Components/Molecules/Bookmarked/Bookmarked.tsx
@@ -5,11 +5,11 @@ import { bookmarkedData } from './Data';
 
 const Bookmarked = () => {
   return (
-    <div className={styles.CategoriesWrapper}>
+    <div className={styles.bookmarkWrapper}>
       <div className={styles.container}>
         <div className={styles.row}>
           {bookmarkedData.map((data, index) => (
-            <div className={styles.categoryCard} key={index}>
+            <div className={styles.bookmarkCard} key={index}>
               <BookmarkedCard
                 cardSubHeading={data.cardSubHeading}
                 cardSubText={data.cardSubText}
