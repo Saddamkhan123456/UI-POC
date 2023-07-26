d67f1a83423cbd8aaf8ac5067ced545caea16bd5
jyotithakre05 <Jyoti.thakre@talentica.com>
2023-07-26 17:10:24 +0530

    view more

---
+++ css-modules/src/Components/Atoms/ViewMore/ViewMore.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ViewMore.module.css';
+
+const ViewMore = () => {
+  return (
+    <a href='/' className={styles.viewmore}>
+      view more
+    </a>
+  );
+};
+
+export default ViewMore;
