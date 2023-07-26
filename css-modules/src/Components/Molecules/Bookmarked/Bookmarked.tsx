3c22805e17b60e1cb709769370a9f3cd3dcc51b2
jyotithakre05 <Jyoti.thakre@talentica.com>
2023-07-26 17:10:38 +0530

    Bookmark minor changes

--- css-modules/src/Components/Molecules/Bookmarked/Bookmarked.tsx
+++ css-modules/src/Components/Molecules/Bookmarked/Bookmarked.tsx
@@ -4,24 +4,30 @@ import BookmarkedCard from '../../Atoms/Card/BookmarkedCard';
 import { bookmarkedData } from './Data';
 import Heading from '../../Atoms/Heading/Heading';
 import Divider from '../../Atoms/Divider/Divider';
+import ViewMore from '../../Atoms/ViewMore/ViewMore';
 
 const Bookmarked = () => {
   return (
-    <div className={styles.bookmarkWrapper}>
-      <Heading label='Bookmarked' />
-      <div className={styles.row}>
-        {bookmarkedData.map((data, index) => (
-          <div className={styles.bookmarkCard} key={index}>
-            <BookmarkedCard
-              cardSubHeading={data.cardSubHeading}
-              cardSubText={data.cardSubText}
-              backgroundImgUrl={data.backgroundImgUrl}
-            />
-          </div>
-        ))}
+    <>
+      <div className={styles.bookmarkWrapper}>
+        <Heading label='Bookmarked' />
+        <div className={styles.row}>
+          {bookmarkedData.map((data, index) => (
+            <div className={styles.bookmarkCard} key={index}>
+              <BookmarkedCard
+                cardSubHeading={data.cardSubHeading}
+                cardSubText={data.cardSubText}
+                backgroundImgUrl={data.backgroundImgUrl}
+              />
+            </div>
+          ))}
+        </div>
+        <div className={styles.viewMoreAlignment}>
+          <ViewMore />
+        </div>
       </div>
       <Divider />
-    </div>
+    </>
   );
 };
 
