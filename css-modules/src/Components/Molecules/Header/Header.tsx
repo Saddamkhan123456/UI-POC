15c8a3c3a743bb5ba2efa3bef1c3ed46aec78b6e
jyotithakre05 <Jyoti.thakre@talentica.com>
2023-07-26 12:50:26 +0530

    Header Added

--- css-modules/src/Components/Molecules/Header/Header.tsx
+++ css-modules/src/Components/Molecules/Header/Header.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import styles from './Header.module.css';
+import Icon from '../../Icons/Icons';
+import Breadcrumb from '../../Atoms/Breadcrumb/Breadcrumb';
+import Search from '../../Atoms/Search/Search';
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+interface HeaderProps {
+  isBreadcrumb?: boolean;
+  isSearch?: boolean;
 }
 
-export default Header
+const Header = ({ isBreadcrumb, isSearch }: HeaderProps) => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.leftHeader}>
+        <a href='/' className={styles.logo}>
+          <Icon kind='logo' size={72} />
+        </a>
+        {isBreadcrumb && <Breadcrumb />}
+      </div>
+      {isSearch && (
+        <div className={styles.rightHeader}>
+          <Search searchVariant='searchSm' isSearchIcon={true} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
