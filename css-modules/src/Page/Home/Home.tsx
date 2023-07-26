474e809f1ca5a71cc8bdfa257df5da7d271c8b1b
jyotithakre05 <Jyoti.thakre@talentica.com>
2023-07-26 17:35:05 +0530

    Added : home page

--- css-modules/src/Page/Home/Home.tsx
+++ css-modules/src/Page/Home/Home.tsx
@@ -4,17 +4,23 @@ import styles from './Home.module.css';
 import Bookmarked from '../../Components/Molecules/Bookmarked/Bookmarked';
 import MoreForYou from '../../Components/Molecules/MoreForYou/MoreForYou';
 import TechDigest from '../../Components/Molecules/TechDigest/TechDigest';
+import Header from '../../Components/Molecules/Header/Header';
+import Banner from '../../Components/Molecules/Banner/Banner';
 
 const Home = () => {
   return (
-    <div>
-      <div className={styles.container}>
-        <Categories />
-        <TechDigest />
-        <Bookmarked />
-        <MoreForYou />
+    <>
+      <Header />
+      <Banner />
+      <div className={styles.homeWrapper}>
+        <div className={styles.container}>
+          <Categories />
+          <TechDigest />
+          <Bookmarked />
+          <MoreForYou />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
