32ee59fc59d04696c3afd249c6b2e2faa835c8cd
jyotithakre05 <Jyoti.thakre@talentica.com>
2023-07-27 14:46:55 +0530

    Article type added in badge component

--- css-modules/src/Components/Atoms/Badge/Badge.tsx
+++ css-modules/src/Components/Atoms/Badge/Badge.tsx
@@ -1,29 +1,42 @@
 import React from 'react';
 import styles from './Badge.module.css';
+import Icon from '../../Icons/Icons';
 
 interface BadgeProps {
   label: string;
+  icon?: string;
+  isIcon?: boolean;
+  size?: number;
 }
 
-const Badge = ({ label }: BadgeProps) => {
+const Badge = ({ label, icon, isIcon, size }: BadgeProps) => {
   const getBackgroundColor = (label: string) => {
     const badgeLabel = label.toLowerCase();
     switch (badgeLabel) {
       case 'medium impact':
-        return 'badge-warning';
+        return styles.badgeWarning;
       case 'high impact':
-        return 'badge-success';
+        return styles.badgeSuccess;
       case 'low impact':
-        return 'badge-light';
+        return styles.badgeLight;
+      case 'hard problem':
+        return `${styles.badgeTransparent} ${styles.hardProblem}`;
+      case 'blogs & tl articles':
+        return `${styles.badgeTransparent} ${styles.blogArticles}`;
       default:
-        return 'badge-light';
+        return styles.badgeTransparent;
     }
   };
 
   const backgroundColor = getBackgroundColor(label);
-  const badgeStyle = `${styles.badge} ${styles[backgroundColor]}`;
+  const badgeStyle = `${styles.badge} ${backgroundColor}`;
 
-  return <span className={badgeStyle}>{label}</span>;
+  return (
+    <span className={badgeStyle}>
+      {isIcon && <Icon kind={icon} size={size} />}
+      {label}
+    </span>
+  );
 };
 
 export default Badge;
