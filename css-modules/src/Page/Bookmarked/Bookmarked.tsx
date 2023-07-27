9a265ead5cfa999311628e4d21ae1c239f5a40e6
jyotithakre05 <Jyoti.thakre@talentica.com>
2023-07-27 17:22:02 +0530

    code refactored

--- css-modules/src/Page/Bookmarked/Bookmarked.tsx
+++ css-modules/src/Page/Bookmarked/Bookmarked.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './Bookmarked.module.css';
 import Layout from '../../Components/Layout/Layout';
 import List from '../../Components/Atoms/List/List';
 import ListItem from '../../Components/Atoms/List/ListItem';
