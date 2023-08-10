c6eb714090b3e8f0dcb8fae48dc124eb31e85236
jyotiT <Jyoti.thakre@talentica.com>
2023-08-10 13:20:16 +0530

    Heading variant added

--- css-modules/src/Components/Atoms/Heading/Heading.tsx
+++ css-modules/src/Components/Atoms/Heading/Heading.tsx
@@ -4,11 +4,13 @@ import styles from './Heading.module.css';
 interface HeadingProps {
   label: string;
   textCenterAlign?: boolean;
+  variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }
 
-const Heading = ({ label, textCenterAlign }: HeadingProps) => {
+const Heading = ({ label, textCenterAlign, variant = 'h3' }: HeadingProps) => {
   const headingClassName = textCenterAlign ? `${styles.heading} ${styles.textCenterAlign}` : styles.heading;
-  return <h3 className={headingClassName}>{label}</h3>;
+  const HeadingTag = variant;
+  return <HeadingTag className={headingClassName}>{label}</HeadingTag>;
 };
 
 export default Heading;
