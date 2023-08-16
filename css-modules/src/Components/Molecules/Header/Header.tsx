90304b1d7342e703712f5851bb8bcd27eba4c002
jyotiT <Jyoti.thakre@talentica.com>
2023-08-16 17:54:22 +0530

    Header stories

--- css-modules/src/Components/Molecules/Header/Header.tsx
+++ css-modules/src/Components/Molecules/Header/Header.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import styles from './Header.module.css';
-import Icon from '../../Icons/Icons';
-import Breadcrumb from '../../Atoms/Breadcrumb/Breadcrumb';
-import Search from '../../Atoms/Search/Search';
+import React from "react";
+import styles from "./Header.module.css";
+import Icon from "../../Icons/Icons";
+import Breadcrumb from "../../Atoms/Breadcrumb/Breadcrumb";
+import Search from "../../Atoms/Search/Search";
 
 interface HeaderProps {
   isBreadcrumb?: boolean;
@@ -13,14 +13,14 @@ const Header = ({ isBreadcrumb, isSearch }: HeaderProps) => {
   return (
     <div className={styles.header}>
       <div className={styles.leftHeader}>
-        <a href='/' className={styles.logo}>
-          <Icon kind='logo' size={72} />
+        <a href="/" className={styles.logo}>
+          <Icon kind="logo" size={72} />
         </a>
         {isBreadcrumb && <Breadcrumb />}
       </div>
       {isSearch && (
         <div className={styles.rightHeader}>
-          <Search searchVariant='searchSm' isSearchIcon={true} />
+          <Search searchVariant="searchSm" isSearchIcon={true} />
         </div>
       )}
     </div>
