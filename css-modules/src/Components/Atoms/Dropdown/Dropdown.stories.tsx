fe4f5f0b245160ff68eba299efd8dd82d6dc7de4
jyotiT <Jyoti.thakre@talentica.com>
2023-08-16 18:48:22 +0530

    Dropdown stories added

---
+++ css-modules/src/Components/Atoms/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Dropdown from "./Dropdown";
+
+const meta = {
+  title: "Atoms/Dropdown",
+  component: Dropdown,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Dropdown>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    dropdownItems: [
+      { value: "languages", label: "Languages" },
+      { value: "WebFrameworks", label: "Web Frameworks" },
+      { value: "Database", label: "Database" },
+      { value: "BigData", label: "BigData" },
+    ],
+    defaultLabel: "Categories",
+  },
+};
--- css-modules/src/Components/Atoms/Dropdown/Dropdown.tsx
+++ css-modules/src/Components/Atoms/Dropdown/Dropdown.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import styles from './Dropdown.module.css';
+import React from "react";
+import styles from "./Dropdown.module.css";
 
 interface DropdownProps {
   defaultLabel: string;
-  dropdownItems: { label: string; value: string }[];
-  value: string;
+  dropdownItems: { value: string; label: string }[];
   className?: string;
 }
 
-const Dropdown = ({ defaultLabel, dropdownItems, value, className }: DropdownProps) => {
+const Dropdown = ({ defaultLabel, dropdownItems, className }: DropdownProps) => {
   return (
-    <select className={`${styles.formSelect} ${className}`} value={value}>
-      <option value=''>{defaultLabel}</option>
-      {dropdownItems.map((item) => (
+    <select className={`${styles.formSelect} ${className}`}>
+      <option value="">{defaultLabel}</option>
+      {dropdownItems.map((item: any, index: number) => (
         <option key={item.value} value={item.value}>
           {item.label}
         </option>
