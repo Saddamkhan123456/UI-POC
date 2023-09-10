abf00fe4260101d218a6a62414d6aa439986634c
palak-tal <45681524+palak-tal@users.noreply.github.com>
2023-09-10 22:38:39 +0530

    carousal style

---
+++ styled-component/styled-component/src/Components/Carousel/Carousel.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Icon from '../../Assets/Icons/Icon';
+
+const CarouselWrapper = styled.div`
+  overflow: hidden;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
+
+const CarouselContent = styled.div`
+  display: flex;
+  transition: transform 0.3s ease-in-out;
+  width: 100%;
+  gap: 30px;
+`;
+
+const CarouselItem = styled.div`
+  flex: 0 0 calc(25% - 30px);
+`;
+
+const SliderButtonPrev = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center; /* Vertically center the button */
+  transform: translateY(-50%); /* Vertically center the button */
+  z-index: 1;
+`;
+
+const SliderButtonNext = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  height: 100%;
+  display: flex;
+  align-items: center; /* Vertically center the button */
+  position: absolute;
+  right: 0;
+  top: 50%; /* Adjust this value to vertically center the button */
+  transform: translateY(-50%); /* Vertically center the button */
+  z-index: 1;
+`;
+
+interface CarouselProps {
+  items: JSX.Element[]; // Define the type of 'items' prop here
+}
+
+const Carousel: React.FC<CarouselProps> = ({ items }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    const newIndex = (currentIndex + 1) % items.length;
+    setCurrentIndex(newIndex);
+  };
+
+  const prevSlide = () => {
+    const newIndex = (currentIndex - 1 + items.length) % items.length;
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <>
+      <CarouselWrapper>
+        <SliderButtonPrev onClick={prevSlide}>
+          <Icon kind='arrow-prev' width={18} height={30} />
+        </SliderButtonPrev>
+        <CarouselContent style={{ transform: `translateX(-${currentIndex * (100 / 4)}%)` }}>
+          {items.map((item, index) => (
+            <CarouselItem key={index}>{item}</CarouselItem>
+          ))}
+        </CarouselContent>
+        <SliderButtonNext onClick={nextSlide}>
+          <Icon kind='arrow-next' width={18} height={30} />
+        </SliderButtonNext>
+      </CarouselWrapper>
+    </>
+  );
+};
+
+export default Carousel;
--- styled-component/styled-component/src/Pages/Home-page/PopularTechDigest.tsx
+++ styled-component/styled-component/src/Pages/Home-page/PopularTechDigest.tsx
@@ -1,27 +1,62 @@
 import React from 'react';
 import Card from '../../Components/Card/Card';
-import { PopularTechDigestData, TEXT_CONSTANTS } from '../../Utils/Constants';
-import { GridContainer } from '../GlobalStyles';
+import { TEXT_CONSTANTS } from '../../Utils/Constants';
 import { Sections } from './styles';
 import { H2 } from '../../Assets/Styles/Utils';
+import Carousel from '../../Components/Carousel/Carousel';
 
 const PopularTechDigestSection = () => {
+  const carouselItems = [
+    <Card
+      key={1}
+      variant={'MediaCard'}
+      heading={'Purescript'}
+      isBackgroundImg={true}
+      backgroundImage={'https://www.talentica.com/wp-content/uploads/2022/10/Blog-Inserting-code-scaled-1.jpg'}
+      centerAlign={true}
+    />,
+    <Card
+      key={2}
+      variant={'MediaCard'}
+      heading={'Apache Kafka'}
+      isBackgroundImg={true}
+      backgroundImage={
+        'https://www.talentica.com/wp-content/uploads/2021/04/prem-baranwal-cryptography-blockchain-blog-1.jpg'
+      }
+      centerAlign={true}
+    />,
+    <Card
+      key={3}
+      variant={'MediaCard'}
+      heading={'Redis'}
+      isBackgroundImg={true}
+      backgroundImage={'https://www.talentica.com/wp-content/uploads/2022/10/blog-fetaure-4.jpg'}
+      centerAlign={true}
+    />,
+    <Card
+      key={4}
+      variant={'MediaCard'}
+      heading={'Aerospike'}
+      isBackgroundImg={true}
+      backgroundImage={'https://www.talentica.com/wp-content/uploads/2022/01/blog-amit-kumar-.jpg'}
+      centerAlign={true}
+    />,
+    <Card
+      key={5}
+      variant={'MediaCard'}
+      heading={'Apache Kafka'}
+      isBackgroundImg={true}
+      backgroundImage={
+        'https://www.talentica.com/wp-content/uploads/2021/04/prem-baranwal-cryptography-blockchain-blog-1.jpg'
+      }
+      centerAlign={true}
+    />,
+  ];
   return (
     <>
       <H2>{TEXT_CONSTANTS.POPULAR_TECH_DIGEST}</H2>
-
       <Sections>
-        <GridContainer columnCount={4}>
-          {PopularTechDigestData.map((item, index) => (
-            <Card
-              variant={'MediaCard'}
-              heading={item.heading}
-              isBackgroundImg={true}
-              backgroundImage={item.backgroundImage}
-              centerAlign={true}
-            />
-          ))}
-        </GridContainer>
+        <Carousel items={carouselItems} />
       </Sections>
     </>
   );
