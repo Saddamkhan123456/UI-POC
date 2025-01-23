b5f59aeb6fc21578f50d147e092b9a5b9b773bda
ShwetaDeodhare <shweta.deodhare@talentica.com>
2025-01-23 16:42:41 +0530

    Updated: font family

--- echarts-poc/src/App.tsx
+++ echarts-poc/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import { BarChart } from "./Charts/Bar";
 import { Card, CardBody, Container } from "react-bootstrap";
 import { LineChart, StackedLinesChart } from "./Charts/Line";
